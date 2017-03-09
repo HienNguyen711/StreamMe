193988dcfa7b2ea1b01fed7d3a4b36dbae576e01
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-03-09 11:54:07 +0200

    update React component

--- components/TweetsApp.js
+++ components/TweetsApp.js
@@ -21,6 +21,44 @@ module.exports = TweetsApp = React.createClass({
   componentWillReceiveProps: function(newProps, oldProps){
     this.setState(this.getInitialState(newProps));
   },
+
+  componentDidMount:function(){
+
+    var self = this;
+    var socket = io.connect();
+    //listen to tweet event
+    socket.on('tweet',function(data){
+      //add a tweet
+      self.addTweet(data);
+    });
+    // Attach scroll event to the window for infinity paging
+    window.addEventListener('scroll', this.checkWindowScroll);
+
+
+  },
+
+  //addTweet
+  addTweet: function(tweet){
+    var updated = this.state.tweets;
+    var count = this.state.count +1;
+    var skip = this.state.skip +1;
+    //add tweet to the beginning of the tweets array
+    updated.unshift(tweet);
+
+    //set state
+    this.setState({
+      tweets:updated,
+      count:count,
+      skip:skip
+    });
+
+
+
+  },
+
+
+
+
   render: function(){
 
     return (
