1c80414eb355b11867019e85dc1a9bc85eb43ab0
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-03-09 10:59:52 +0200

    TweetsApp component

---
+++ components/TweetsApp.js
@@ -0,0 +1,35 @@
+var React = require('react');
+
+module.exports = TweetsApp = React.createClass({
+
+  getInitialState: function(props){
+
+    props = props || this.props;
+
+    // Set initial application state using props
+    return {
+      tweets: props.tweets,
+      count: 0,
+      page: 0,
+      paging: false,
+      skip: 0,
+      done: false
+    };
+
+  },
+
+  componentWillReceiveProps: function(newProps, oldProps){
+    this.setState(this.getInitialState(newProps));
+  },
+  render: function(){
+
+    return (
+      <div className="tweets-app">
+        <Tweets tweets={this.state.tweets} />
+        <Loader paging={this.state.paging}/>
+        <NotificationBar count={this.state.count} onShowNewTweets={this.showNewTweets}/>
+      </div>
+    )
+
+  }
+});
