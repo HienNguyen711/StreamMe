6bea181211d2dd6754dc277055ae74a68c02cb2c
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-03-09 10:49:56 +0200

    server render

--- routes.js
+++ routes.js
@@ -5,24 +5,41 @@ var Tweet = require('./models/Tweet');
 
 module.exports = {
 
+  //index
+  index: function (req, res) {
+    //call static model method to get tweets in the db
+    Tweet.getTweets(0, 0, function (tweets, pages) {
+      //server render
+      //render react to a string passing in our fetched tweets
+      var markup = React.renderComponentToString(
+
+        TweetsApp({
+          tweets: tweets
+        })
+
+      );
+      //render HOME template
+      res.render('home', {
+        markup: markup, //pass rendered react markup
+        state: JSON.stringify(tweets); //pass current state to client side
+
+      });
 
 
 
 
 
+    });
 
 
 
-
-
-
-
+  },
 
 
   //page
-  page: function(req,res){
+  page: function (req, res) {
     //fetch tweets by page via params
-    Tweet.getTweets(req.params.page,req.params.skip,function(tweets){
+    Tweet.getTweets(req.params.page, req.params.skip, function (tweets) {
       //render json
       res.json(tweets);
 
