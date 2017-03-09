860471c4e349d8f82b771fb0cb61f7cd58071054
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-03-09 10:29:04 +0200

    query ..

--- models/Tweet.js
+++ models/Tweet.js
@@ -10,6 +10,29 @@ var schema = new mongoose.Schema({
     screenname : String
 
 
-
-
 });
+
+schema.statics.getTweets = function(page,skip, cb){
+  var tweets = [];
+  var start = (page*10)+(skip*1);
+
+  //query the db
+  Tweet.find({},'twid active author avatar body date screenname',{skip: start, limit: 10}).sort({date: 'desc'}).exec(function(err,docs){
+
+   //if no error
+    if(!err) {
+      tweets = docs;
+      tweets.forEach(function(tweet){
+        tweet.active = true; //set tweet is active
+      });
+    }
+
+    // Pass them back to the specified callback
+    cb(tweets);
+
+  });
+
+};
+
+// Return a Tweet model based upon the defined schema
+module.exports = Tweet = mongoose.model('Tweet', schema);
